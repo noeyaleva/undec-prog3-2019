3443c4302d602fc8ea24696eb3809f855166de3e
noeyaleva <noeadanyaleva@gmail.com>
2019-04-28 16:20:34 -0300

    se crea el test de la Aerolinea

--- src/test/java/test/TestAerolinea.java
+++ src/test/java/test/TestAerolinea.java
@@ -4,11 +4,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import dominio.Contacto;
+import dominio.exceptions.ExceptionContactoMalFormado;
+import dominio.exceptions.ExceptionGeneral;
+
 class TestAerolinea {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAerolinea_NombreBienFormado() {
+		Aerolinea a1 = Aerolinea(1, Argentinas);
+		assertEquals(" ID: 1 - Nombre: Argentinas",a1.toString());
+	}
+	
+	@Test
+	void testNombreMalFormado() {
+		try {
+			Aerolinea a1 = Aerolinea(1, "");
+			fail("Esta linea no deberia correrse");
+		} catch(ExceptionContactoMalFormado e) {
+			assertEquals("Error en Aerolinea", e.getMessage());
+		} catch(ExceptionGeneral e) {
+			fail("Esta linea no deberia correrse");
+		} catch(Exception e) {
+			fail("Esta linea no deberia correrse");
+		}
+		
+		try {
+			Aerolinea a1 = Aerolinea(1, null);
+			fail("Esta linea no deberia correrse");
+		} catch(ExceptionNombreMalFormado e) {
+			assertEquals("Error en el nombre", e.getMessage());
+		} catch(ExceptionGeneral e) {
+			fail("Esta linea no deberia correrse");
+		} catch(Exception e) {
+			fail("Esta linea no deberia correrse");
+		}
+		
+	}
+	
+	@Test
+	void testIdMalFormado() {
+		try {
+			Aerolinea a1 = Aerolinea(-1, "Argentinas");
+			fail("Esta linea no deberia correrse");
+		} catch (ExceptionIdMalFormado e) {
+			asserEquals("Error en el Id.", e.getMessage());
+		} catch (ExceptionGeneral e) {
+			fail("Esta linea no deberia correrse.");
+		} catch (Exception e) {
+			fail("Esta linea no deberia correrse.");
+		}
+		
 	}
 
 }
