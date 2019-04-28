f08d605e8f54dea2dc4b29e25f501b465a91cbbe
noeyaleva <noeadanyaleva@gmail.com>
2019-04-28 15:46:37 -0300

    asi inicia el proyecto.

---
+++ src/test/java/test/TestAerolinea.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAerolinea {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
